22a0db8c1bf24defb397e948032a6f4bc46a24ac
Quentin Sollet <qsollet@users.noreply.github.com>
2020-04-10 15:58:48 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -9,13 +9,13 @@ DISCORD_TOKEN = config('DISCORD_TOKEN')
 LISTS = {}
 bot = commands.Bot(command_prefix='>')
 
-@bot.command()
-async def ping(ctx):
+@bot.command(name='ping')
+async def _ping(ctx):
     await ctx.send('pong')
 
 # command add :list :item
-@bot.command()
-async def add(ctx, key, item):
+@bot.command(name='add')
+async def _add(ctx, key, item):
     if key not in LISTS:
         LISTS[key] = []
     if item not in LISTS[key]:
@@ -23,8 +23,8 @@ async def add(ctx, key, item):
     await ctx.send('saved')
 
 # command remove :list :item
-@bot.command()
-async def remove(ctx, key, item):
+@bot.command(name='remove')
+async def _remove(ctx, key, item):
     try:
         LISTS[key].remove(item)
         await ctx.send('saved')
@@ -32,18 +32,18 @@ async def remove(ctx, key, item):
         await ctx.send('{} not found'.format(item))
 
 # TODO command pick :list
-@bot.command()
-async def pick(ctx, key):
+@bot.command(name='pick')
+async def _pick(ctx, key):
     try:
         await ctx.send(random.choice(LISTS[key]))
     except:
         await ctx.send('List not found')
 
 # TODO command list :list
-@bot.command()
-async def list(ctx, key):
+@bot.command(name='list')
+async def _list(ctx, key):
     try:
-        await ctx.send(random.choice(LISTS[key]))
+        await ctx.send('\n'.join(LISTS[key]))
     except:
         await ctx.send('List not found')
 
