a1fdf0aa0e39f3f28cdfb214e316df3758f60ff0
Quentin Sollet <quentin@sollet.fr>
2020-04-10 17:24:57 +0200

    can now add or remove multiples items at the same time

--- src/main.py
+++ src/main.py
@@ -6,47 +6,53 @@ from discord.ext import commands
 DISCORD_TOKEN = config('DISCORD_TOKEN')
 DATAFILE_PATH = config('DATAFILE_PATH')
 
-# TODO save list in a file
+# TODO save list in a file, pickle?
 LISTS = {}
 
 bot = commands.Bot(command_prefix='>')
 
-@bot.command(name='ping')
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} is now connected to Discord')
+
+@bot.command(name='ping', help='just replies pong')
 async def _ping(ctx):
     await ctx.send('pong')
 
 # command add :list :item
-@bot.command(name='add')
-async def _add(ctx, key, item):
-    if key not in LISTS:
-        LISTS[key] = []
-    if item not in LISTS[key]:
-        LISTS[key].append(item)
-    await ctx.send('saved')
+@bot.command(name='add', help='Add one or many item to a list')
+async def _add(ctx, list_name, *items):
+    if list_name not in LISTS:
+        LISTS[list_name] = []
+    for item in items:
+        if item not in LISTS[list_name]:
+            LISTS[list_name].append(item)
+    await ctx.send('saved `{}` in list `{}`'.format(', '.join(items), list_name))
 
 # command remove :list :item
-@bot.command(name='remove')
-async def _remove(ctx, key, item):
+@bot.command(name='remove', help='Remove one or many item from a list')
+async def _remove(ctx, list_name, *items):
     try:
-        LISTS[key].remove(item)
-        await ctx.send('saved')
+        for item in items:
+            LISTS[list_name].remove(item)
+        await ctx.send('removed `{}` in list `{}`'.format(', '.join(items), list_name))
     except:
-        await ctx.send('{} not found'.format(item))
+        await ctx.send('`{}` not found'.format(item))
 
 # command pick :list
-@bot.command(name='pick')
-async def _pick(ctx, key):
+@bot.command(name='pick', help='Pick an item randomly from a list')
+async def _pick(ctx, list_name):
     try:
-        await ctx.send(random.choice(LISTS[key]))
+        await ctx.send(random.choice(LISTS[list_name]))
     except:
-        await ctx.send('List not found')
+        await ctx.send('List `{}` not found'.format(list_name))
 
 # command list :list
-@bot.command(name='list')
-async def _list(ctx, key):
+@bot.command(name='list', help='Display all items of a list')
+async def _list(ctx, list_name):
     try:
-        await ctx.send('\n'.join(LISTS[key]))
+        await ctx.send('\n'.join(['{}. {}'.format(i+1, x) for i,x in enumerate(LISTS[list_name])]))
     except:
-        await ctx.send('List not found')
+        await ctx.send('List `{}` not found'.format(list_name))
 
 bot.run(DISCORD_TOKEN)
